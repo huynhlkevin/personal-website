16a9853923038aa15967e2551403fc75183908d2
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-11-25 19:39:09 -0800

    add title and sortOrder properties

--- src/content/about/about-education.md
+++ src/content/about/about-education.md
@@ -1,4 +1,6 @@
 ---
+title: "Education"
+sortOrder: 1
 ---
 ### California State University of Long Beach -- December 2014
 **Bachelor of Science, Electrical Engineering** with an emphasis on control systems. 
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,12 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
 const aboutCollection = defineCollection({
-  type: 'content'
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    sortOrder: z.number()
+  })
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
