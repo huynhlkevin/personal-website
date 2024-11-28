46fb28ea8d19c2c065ea924cf9a01e4b83c80493
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-11-27 19:25:39 -0800

    add api tests

--- tests/visitor-counter.spec.ts
+++ tests/visitor-counter.spec.ts
@@ -1,16 +1,38 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('check visitor count increment', async ({ page }) => {
-  await page.goto('https://www.huynhlkevin.com/');
+test("check visitor counter API CORS headers", async () => {
+  const response = await getResponse();
+  expect(response.headers.get("access-control-allow-headers")).toBe("Content-Type")
+  expect(response.headers.get("access-control-allow-origin")).toBe("https://www.huynhlkevin.com")
+  expect(response.headers.get("access-control-allow-methods")).toBe("POST")
+});
+
+test("check visitor counter API increment", async () => {
+  const firstVisitorCounterValue = await getVisitorCounterValue();
+  const secondVisitorCounterValue = await getVisitorCounterValue();
+  expect(secondVisitorCounterValue).toBe(firstVisitorCounterValue + 1);
+});
+
+test("check visitor counter increment", async ({ page }) => {
+  await page.goto("https://www.huynhlkevin.com/");
 
   const textContentBeforeReload = await page.getByText(/Visitor #\d+/).textContent();
-  const visitorCountBeforeReload = Number(textContentBeforeReload?.split('#')[1]);
+  const visitorCountBeforeReload = Number(textContentBeforeReload?.split("#")[1]);
 
   // Reload the page to trigger the visitor counter increment
   await page.reload();
 
   const textContentAfterReload = await page.getByText(/Visitor #\d+/).textContent();
-  const visitorCountAfterReload = Number(textContentAfterReload?.split('#')[1]);
+  const visitorCountAfterReload = Number(textContentAfterReload?.split("#")[1]);
 
   expect(visitorCountAfterReload).toBe(visitorCountBeforeReload + 1);
 });
+
+async function getResponse(): Promise<Response> {
+  return await fetch("https://f36g81unmd.execute-api.us-west-1.amazonaws.com/visit/", { method: "POST" });
+}
+
+async function getVisitorCounterValue(): Promise<number> {
+  const response = await fetch("https://f36g81unmd.execute-api.us-west-1.amazonaws.com/visit/", { method: "POST" });
+  return (await response.json())["data"];
+}
