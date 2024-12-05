9ae2ef359bffbe9ed11d70f5fab3a78fe68afb95
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-04 22:12:17 -0800

    Add tests to development workflow (#13)
    * use env variable for the website url
    
    * add website tests workflow

--- tests/visitor-counter.spec.ts
+++ tests/visitor-counter.spec.ts
@@ -1,20 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test("check visitor counter API CORS headers", async () => {
-  const response = await getResponse();
-  expect(response.headers.get("access-control-allow-headers")).toBe("Content-Type")
-  expect(response.headers.get("access-control-allow-origin")).toBe("https://www.huynhlkevin.com")
-  expect(response.headers.get("access-control-allow-methods")).toBe("POST")
-});
-
-test("check visitor counter API increment", async () => {
-  const firstVisitorCounterValue = await getVisitorCounterValue();
-  const secondVisitorCounterValue = await getVisitorCounterValue();
-  expect(secondVisitorCounterValue).toBe(firstVisitorCounterValue + 1);
-});
-
 test("check visitor counter increment", async ({ page }) => {
-  await page.goto("https://www.huynhlkevin.com/");
+  await page.goto(process.env.WEBSITE_URL!);
 
   const textContentBeforeReload = await page.getByText(/Visitor #\d+/).textContent();
   const visitorCountBeforeReload = Number(textContentBeforeReload?.split("#")[1]);
@@ -27,20 +14,3 @@ test("check visitor counter increment", async ({ page }) => {
 
   expect(visitorCountAfterReload).toBe(visitorCountBeforeReload + 1);
 });
-
-async function getResponse(): Promise<Response> {
-  return await fetch(
-    "https://f36g81unmd.execute-api.us-west-1.amazonaws.com/visit/", 
-    { 
-      method: "POST",
-      headers: {
-        "x-api-key": "ovAG1tOEin4Y9eYBDVKQz3UzP2AzGvv02UFehNIY"
-      }
-    }
-  );
-}
-
-async function getVisitorCounterValue(): Promise<number> {
-  const response = await getResponse();
-  return (await response.json())["data"];
-}
