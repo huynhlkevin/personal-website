dcd418d8e2f8b9bc326b902be326bc5c13c06599
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-08 18:14:11 -0800

    add blog description

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,7 +14,8 @@ const blogCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    date: z.string()
+    date: z.string(),
+    description: z.string()
   })
 });
 
