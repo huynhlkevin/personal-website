2185390b1df6e9128de76685e5c1d0687d5409c6
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-09 19:48:15 -0800

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   env: {
@@ -8,5 +10,7 @@ export default defineConfig({
       REST_API_ENDPOINT: envField.string({ context: "client", access: "public", default: "" }),
       REST_API_KEY: envField.string({ context: "client", access: "public", default: "" })
     }
-  }
+  },
+
+  integrations: [mdx()]
 });
