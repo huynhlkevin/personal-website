3c6609ec029f0fd9757cf6bfdef890c115b44bcd
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-09 17:37:44 -0800

    update to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    env: {
-      schema: {
-        REST_API_ENDPOINT: envField.string({ context: "client", access: "public", default: "" }),
-        REST_API_KEY: envField.string({ context: "client", access: "public", default: "" })
-      }
+  env: {
+    schema: {
+      REST_API_ENDPOINT: envField.string({ context: "client", access: "public", default: "" }),
+      REST_API_KEY: envField.string({ context: "client", access: "public", default: "" })
     }
   }
 });
