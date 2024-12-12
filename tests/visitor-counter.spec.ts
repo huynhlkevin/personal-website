df2d656743d1b1c6d0de8237944754367483ad1e
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-11 22:50:26 -0800

    Always display the total number of visitors. (#18)

--- tests/visitor-counter.spec.ts
+++ tests/visitor-counter.spec.ts
@@ -3,14 +3,14 @@ import { test, expect } from "@playwright/test";
 test("check visitor counter increments when new visitor", async ({ page }) => {
   await page.goto(process.env.WEBSITE_URL!);
 
-  const textContentBeforeReload = await page.getByText(/Visitor #\d+/).textContent();
-  await page.evaluate(() => localStorage.removeItem("visitor"));
+  const textContentBeforeReload = await page.getByText(/Visitors: \d+/).textContent();
+  await page.evaluate(() => localStorage.removeItem("visited"));
   const visitorCountBeforeReload = Number(textContentBeforeReload?.split("#")[1]);
 
   // Reload the page to trigger the visitor counter increment
   await page.reload();
 
-  const textContentAfterReload = await page.getByText(/Visitor #\d+/).textContent();
+  const textContentAfterReload = await page.getByText(/Visitors: \d+/).textContent();
   const visitorCountAfterReload = Number(textContentAfterReload?.split("#")[1]);
 
   expect(visitorCountAfterReload).toBe(visitorCountBeforeReload + 1);
@@ -19,13 +19,13 @@ test("check visitor counter increments when new visitor", async ({ page }) => {
 test("check visitor counter increments when not new visitor", async ({ page }) => {
   await page.goto(process.env.WEBSITE_URL!);
 
-  const textContentBeforeReload = await page.getByText(/Visitor #\d+/).textContent();
+  const textContentBeforeReload = await page.getByText(/Visitors: \d+/).textContent();
   const visitorCountBeforeReload = Number(textContentBeforeReload?.split("#")[1]);
 
   // Reload the page to trigger the visitor counter increment
   await page.reload();
 
-  const textContentAfterReload = await page.getByText(/Visitor #\d+/).textContent();
+  const textContentAfterReload = await page.getByText(/Visitors: \d+/).textContent();
   const visitorCountAfterReload = Number(textContentAfterReload?.split("#")[1]);
 
   expect(visitorCountAfterReload).toBe(visitorCountBeforeReload);
