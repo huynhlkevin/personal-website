42f85c655d0c3ce536c9d43169d4eb6dddb083c3
Kevin Huynh <7540031+huynhlkevin@users.noreply.github.com>
2024-12-11 18:28:28 -0800

    add new visitor counter test

--- tests/visitor-counter.spec.ts
+++ tests/visitor-counter.spec.ts
@@ -1,8 +1,23 @@
 import { test, expect } from "@playwright/test";
 
-test("check visitor counter increment", async ({ page }) => {
+test("check visitor counter increments when new visitor", async ({ page }) => {
   await page.goto(process.env.WEBSITE_URL!);
-  await page.evaluate(() => localStorage.removeItem("visited"));
+
+  const textContentBeforeReload = await page.getByText(/Visitor #\d+/).textContent();
+  await page.evaluate(() => localStorage.removeItem("visitor"));
+  const visitorCountBeforeReload = Number(textContentBeforeReload?.split("#")[1]);
+
+  // Reload the page to trigger the visitor counter increment
+  await page.reload();
+
+  const textContentAfterReload = await page.getByText(/Visitor #\d+/).textContent();
+  const visitorCountAfterReload = Number(textContentAfterReload?.split("#")[1]);
+
+  expect(visitorCountAfterReload).toBe(visitorCountBeforeReload + 1);
+});
+
+test("check visitor counter increments when not new visitor", async ({ page }) => {
+  await page.goto(process.env.WEBSITE_URL!);
 
   const textContentBeforeReload = await page.getByText(/Visitor #\d+/).textContent();
   const visitorCountBeforeReload = Number(textContentBeforeReload?.split("#")[1]);
@@ -13,5 +28,5 @@ test("check visitor counter increment", async ({ page }) => {
   const textContentAfterReload = await page.getByText(/Visitor #\d+/).textContent();
   const visitorCountAfterReload = Number(textContentAfterReload?.split("#")[1]);
 
-  expect(visitorCountAfterReload).toBe(visitorCountBeforeReload + 1);
+  expect(visitorCountAfterReload).toBe(visitorCountBeforeReload);
 });
